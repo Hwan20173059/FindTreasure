6f7a49857679649df4c28f603862ea47b5debbb5
soonseong.k <taetae8939@gmail.com>
2024-02-26 16:34:39 +0900

    Feat: 하단 점프 개선

--- Assets/_Script/Tile/TileControl.cs
+++ Assets/_Script/Tile/TileControl.cs
@@ -1,29 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 using UnityEngine.InputSystem;
 
 public class TileControl : MonoBehaviour
 {
-    [SerializeField] private PlatformEffector2D platformObject;
-    [SerializeField] private GameObject player;
+    private PlatformEffector2D platformObject;
+    private bool _playerOnPlatform;
 
     private void Start()
     {
-        //platformObject = GetComponent<PlatformEffector2D>();
+        platformObject = GetComponent<PlatformEffector2D>();
     }
 
-    //public void OnDown(InputAction.CallbackContext context)
-    //{
-    //    Debug.Log(context);
-    //    if (context.phase == InputActionPhase.Performed)
-    //    {
-    //        platformObject.rotationalOffset = 180f;
-    //    }
-    //    else if (context.phase == InputActionPhase.Canceled)
-    //    {
-    //        platformObject.rotationalOffset = 0f;
-    //    }
-    //}
+    public void OnDown(InputAction.CallbackContext context)
+    {
+        //Debug.Log(context);
+        if (context.phase == InputActionPhase.Performed)
+        {
+            platformObject.rotationalOffset = 180f;
+        }
+    }
+
+    private void SetPlayerOnPlatform(Collision2D other, bool value)
+    {
+        var player = other.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            platformObject.rotationalOffset = 0f;
+            _playerOnPlatform = value;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SetPlayerOnPlatform(collision, true);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        SetPlayerOnPlatform(collision, false);
+    }
+
 }

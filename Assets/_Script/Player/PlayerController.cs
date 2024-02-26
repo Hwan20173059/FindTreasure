d1ed62ddfd695246f3dc0b0ac682b3541c5cd5b1
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-02-26 17:59:46 +0900

    Fix. 플레이어컨트롤러 스크립트 정리

--- Assets/_Script/Player/PlayerController.cs
+++ Assets/_Script/Player/PlayerController.cs
@@ -19,11 +19,8 @@ public class PlayerController : MonoBehaviour
 
     [Header("Layer")]
     [SerializeField] private LayerMask ground;
-    [SerializeField] private LayerMask passthrough;
     float raycastDistance = 0.1f;
 
-    private PlatformEffector2D platformObject;
-    private bool _playerOnPlatform; // 얕은 플랫폼 위에 있는지
 
     private void Awake()
     {
@@ -72,52 +69,6 @@ public class PlayerController : MonoBehaviour
         if (!onJump)
         {
             _rigidbody.AddForce(Vector2.up * playerJumpPower, ForceMode2D.Impulse);
-        }
-    }
-
-
-
-
-    /// <summary>
-    /// S 키를 누르면 얕은 플랫폼에서 내려온다
-    /// </summary>
-    public void OnDown(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
-        {
-            if (_playerOnPlatform)
-            {
-                Debug.Log("platformObject");
-                platformObject.rotationalOffset = 180f;
-            }
-        }
-    }
-
-    private void SetPlayerOnPlatform(Collision2D other, bool value)
-    {
-        platformObject = other.gameObject.GetComponent<PlatformEffector2D>();
-        if (platformObject != null)
-        {
-            platformObject.rotationalOffset = 0f;
-            _playerOnPlatform = value;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //Debug.Log("OnCollisionEnter2D");
-        if (passthrough.value == (passthrough.value | (1 << collision.gameObject.layer)))
-        {
-            SetPlayerOnPlatform(collision, true);
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        //Debug.Log("OnCollisionExit2D");
-        if (passthrough.value == (passthrough.value | (1 << collision.gameObject.layer)))
-        {
-            SetPlayerOnPlatform(collision, false);
         }
     }
 

378744800af0306b133f25e8adf8496e3b0df93a
hygge31 <yiuter1@hanmail.net>
2024-02-28 09:55:34 +0900

    .

--- Assets/_Script/Item/Bomb/BombExplosion.cs
+++ Assets/_Script/Item/Bomb/BombExplosion.cs
@@ -9,15 +9,15 @@ public class BombExplosion : MonoBehaviour
 
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Vector2 dir = (collision.transform.position - transform.position).normalized;
-            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
-            playerController.playerStats.TakeDamage(bombDamage);
-            playerController._rigidbody.AddForce(dir * 90, ForceMode2D.Impulse);
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        Vector2 dir = (collision.transform.position - transform.position).normalized;
+    //        PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+    //        playerController.playerStats.TakeDamage(bombDamage);
+    //        playerController._rigidbody.AddForce(dir * 90, ForceMode2D.Impulse);
+    //    }
+    //}
 }
 

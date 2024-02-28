e6d9c4c9eb90a6828ba0926b1259abad052d040b
soonseong.k <taetae8939@gmail.com>
2024-02-28 12:11:16 +0900

    Chore: 주석 정리

--- Assets/_Script/Item/ChestInteract.cs
+++ Assets/_Script/Item/ChestInteract.cs
@@ -27,21 +27,6 @@ public class ChestInteract : MonoBehaviour
         state = ChestState.Close;
     }
 
-    //private void OnTriggerStay2D(Collider2D collision)
-    //{
-    //    if (layerMask.value == (layerMask.value | (1 << collision.gameObject.layer)))
-    //    {
-    //        if (state == ChestState.Close)
-    //        {
-    //            // Open
-    //        }
-    //        else
-    //        {
-    //            // 열쇠 획득 => 플레이어로 옮김
-    //        }
-    //    }
-    //}
-
     public void SetChestState(ChestState _state)
     {
         if (state != _state)
@@ -78,29 +63,18 @@ public class ChestInteract : MonoBehaviour
     IEnumerator MoveKeyObject()
     {
         // 좀 더 이쁘게 움직일수 있다면 좋을듯.
-        Debug.Log("MoveKeyObject");
-
         Vector3 destination = new Vector3(13, 7, 0);
         float moveSpeed = 3f;
         SpriteRenderer keyObject = rewardsObject.GetComponentInChildren<SpriteRenderer>();
 
         while (Vector3.Distance(keyObject.transform.position, destination) > 0.5f)
         {
-            Debug.Log(Vector3.Distance(keyObject.transform.position, destination));
-            // 현재 위치에서 목적지까지 이동하는 방향 벡터 계산
             Vector3 direction = (destination - keyObject.transform.position).normalized;
-
-            // 이동
             keyObject.transform.position += direction * moveSpeed * Time.deltaTime;
             moveSpeed += 0.3f;
-            // 다음 프레임까지 대기
             yield return null;
         }
-
-        // 목적지에 도착하면 위치를 목적지로 설정 (오차 보정)
         keyObject.transform.position = destination;
-
-        Debug.Log("도착!");
         rewardsObject.SetActive(false);
     }
 }

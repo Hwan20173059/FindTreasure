17f0520c5331dfea70fd0b983730284afe44f67d
EcchiClone <aa01092308481@gmail.com>
2024-02-28 10:14:22 +0900

    chore, 몬스터 스폰 데이터를 SO로 관리 및 자잘한 수정

---
+++ Assets/_Script/Monster/CAnimationHandler.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CAnimationHandler : MonoBehaviour
+{
+    #region Fields
+    #endregion Fields
+
+    #region Members
+    private Animator m_Animator;
+    [SerializeField]
+    private MonsterController m_MonsterController;
+
+    #endregion Members
+
+
+    #region Methods
+    void Awake()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_MonsterController = GetComponentInParent<MonsterController>();
+    }
+
+    //public void EnterNextScene()
+    //{
+    //    // 애니메이션 재생
+    //    m_Animator.Play("Animation_Name");
+    //}
+
+    public void AtAttack()
+    {
+        Debug.Log("범위 내 플레이어에 데미지");
+    }
+    public void AtEndAnimation()
+    {
+        Debug.Log("애니메이션 종료");
+        m_MonsterController.AtEndAnimation();
+    }
+
+    #endregion Methods
+}

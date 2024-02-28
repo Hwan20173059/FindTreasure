9d05162b08c8d9ee6b2673ff71ef4a1a8d4777bf
EcchiClone <aa01092308481@gmail.com>
2024-02-28 19:02:48 +0900

    feat. 몬스터 공격 딜레이 적용, 소소한 버그 픽스

--- Assets/_Script/Monster/MonsterBaseController.cs
+++ Assets/_Script/Monster/MonsterBaseController.cs
@@ -25,7 +25,6 @@ public class MonsterBaseController : MonoBehaviour
     [SerializeField]
     protected GameObject _lockTarget;
 
-
     protected MonsterStat _stat;
 
     [SerializeField]
@@ -33,6 +32,7 @@ public class MonsterBaseController : MonoBehaviour
 
     public float CurrentMoveDirection { get { return _currentMoveDirection; } set { _currentMoveDirection = value; } }
 
+    private float _lastAttackTime = -Mathf.Infinity;
 
     public virtual MonsterState State
     {
@@ -97,11 +97,20 @@ public class MonsterBaseController : MonoBehaviour
         //    Managers.UI.MakeWorldSpaceUI<UI_HPBar>(transform);
     }
 
-    protected virtual void UpdateTracking() { }
+    protected virtual void UpdateAttack()
+    {
+        if (Time.time - _lastAttackTime > _stat.AttackDelay)
+        {
+            // 공격 중에도 플레이어의 위치에 따라 몬스터의 XFlip 조정
+            _destPos = _lockTarget.transform.position;
+            Vector3 dir = _destPos - transform.position;
+            //Debug.Log(dir.x);
+            CurrentMoveDirection = dir.x > 0 ? 1f : -1f;
+            transform.Find("Sprite").GetComponent<SpriteRenderer>().flipX = dir.x > 0 ? false : true;
 
-    protected virtual void UpdateIdle() { }
-
-    protected virtual void UpdateAttack() { }
+            _lastAttackTime = Time.time;
+        }
+    }
 
     protected virtual void UpdateDie()
     {
@@ -187,10 +196,10 @@ public class MonsterBaseController : MonoBehaviour
     {
         if (_lockTarget != null)
         {
-            float distance = (_lockTarget.transform.position - transform.position).magnitude;
-            if (distance <= _stat.AttackActionRange)
-                State = MonsterState.Attack;
-            else
+            //float distance = (_lockTarget.transform.position - transform.position).magnitude;
+            //if (distance <= _stat.AttackActionRange)
+            //    State = MonsterState.Attack;
+            //else
                 State = MonsterState.Track;
         }
         else
@@ -207,5 +216,79 @@ public class MonsterBaseController : MonoBehaviour
 
         GameObject projectile = Instantiate(ProjectilePrefab, spawnPosition, Quaternion.identity);
         projectile.GetComponent<MonsterProjectile>().damage = _stat.AttackDamage;
+    }
+    protected virtual void UpdateTracking()
+    {
+        //Debug.Log("Monster UpdateTracking");
+
+        // 플레이어가 내 사정거리보다 가까우면 공격, 멀어지면 Idle로 전환
+        if (_lockTarget != null)
+        {
+            _destPos = _lockTarget.transform.position;
+            float distance = (_destPos - transform.position).magnitude;
+            if (distance <= _stat.AttackActionRange)
+            {
+                if (Time.time - _lastAttackTime > _stat.AttackDelay)
+                {
+                    State = MonsterState.Attack;
+                    _lastAttackTime = Time.time;
+                }
+                return;
+            }
+            if (distance > _stat.ScanRange)
+            {
+                _lockTarget = null;
+                State = MonsterState.IdleStop;
+                return;
+            }
+        }
+
+        // 이동
+        Vector3 dir = _destPos - transform.position;
+        if (Mathf.Abs(dir.x) >= 0.05f)
+        {
+            // dir.x 가 음인지 양인지 확인하여 이동
+            CurrentMoveDirection = dir.x > 0 ? 1f : -1f;
+            transform.Find("Sprite").GetComponent<SpriteRenderer>().flipX = dir.x > 0 ? false : true;
+
+            // 예상 위치 계산 후 위치가 MinX와 MaxX 사이에 있는지 확인하고 조정
+            Vector3 nextPosition = transform.position + Vector3.right * _stat.MoveSpeed * CurrentMoveDirection * Time.deltaTime;
+            nextPosition.x = Mathf.Clamp(nextPosition.x, _stat.MinX, _stat.MaxX);
+
+            if (!_stat.IsStopOnTrack)
+                transform.position = nextPosition;
+        }
+    }
+    protected virtual void UpdateIdle()
+    {
+        //Debug.Log("Monster UpdateIdle");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) return;
+
+        float distance = (player.transform.position - transform.position).magnitude;
+        if (distance <= _stat.ScanRange)
+        {
+            _lockTarget = player;
+            State = MonsterState.Track;
+            return;
+        }
+
+        // 현재 위치가 MinX보다 작거나 MaxX보다 클 경우 방향 전환
+        if (transform.position.x <= _stat.MinX || transform.position.x >= _stat.MaxX)
+        {
+            if (!_stat.IsStopOnIdle)
+                _currentMoveDirection *= -1;
+        }
+
+        // 스프라이트의 방향 설정
+        transform.Find("Sprite").GetComponent<SpriteRenderer>().flipX = _currentMoveDirection < 0;
+
+        // 몬스터 이동
+        if (!_stat.IsStopOnIdle)
+            transform.position += Vector3.right * _currentMoveDirection * _stat.MoveSpeed * Time.deltaTime;
+
+        // 몬스터가 범위를 벗어나지 않도록 조정
+        float clampedX = Mathf.Clamp(transform.position.x, _stat.MinX, _stat.MaxX);
+        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 }
--- Assets/_Script/Monster/MonsterRange.cs
+++ Assets/_Script/Monster/MonsterRange.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static MonsterBaseController;
 
 public class MonsterRange : MonsterBaseController
 {
@@ -9,75 +8,5 @@ public class MonsterRange : MonsterBaseController
     protected override void Init()
     {
         base.Init();
-    }
-    protected override void UpdateIdle()
-    {
-        //Debug.Log("Monster UpdateIdle");
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player == null) return;
-
-        float distance = (player.transform.position - transform.position).magnitude;
-        if (distance <= _stat.ScanRange)
-        {
-            _lockTarget = player;
-            State = MonsterState.Track;
-            return;
-        }
-
-        // 현재 위치가 MinX보다 작거나 MaxX보다 클 경우 방향 전환
-        if (transform.position.x <= _stat.MinX || transform.position.x >= _stat.MaxX)
-        {
-            if (!_stat.IsStopOnIdle)
-                _currentMoveDirection *= -1;
-        }
-
-        // 스프라이트의 방향 설정
-        transform.Find("Sprite").GetComponent<SpriteRenderer>().flipX = _currentMoveDirection < 0;
-
-        // 몬스터 이동
-        if (!_stat.IsStopOnIdle)
-            transform.position += Vector3.right * _currentMoveDirection * _stat.MoveSpeed * Time.deltaTime;
-
-        // 몬스터가 범위를 벗어나지 않도록 조정
-        float clampedX = Mathf.Clamp(transform.position.x, _stat.MinX, _stat.MaxX);
-        transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
-    }
-    protected override void UpdateTracking()
-    {
-        //Debug.Log("Monster UpdateTracking");
-
-        // 플레이어가 내 사정거리보다 가까우면 공격, 멀어지면 Idle로 전환
-        if (_lockTarget != null)
-        {
-            _destPos = _lockTarget.transform.position;
-            float distance = (_destPos - transform.position).magnitude;
-            if (distance <= _stat.AttackActionRange)
-            {
-                State = MonsterState.Attack;
-                return;
-            }
-            if (distance > _stat.ScanRange)
-            {
-                _lockTarget = null;
-                State = MonsterState.IdleStop;
-                return;
-            }
-        }
-
-        // 이동
-        Vector3 dir = _destPos - transform.position;
-        if (Mathf.Abs(dir.x) >= 0.05f)
-        {
-            // dir.x 가 음인지 양인지 확인하여 이동
-            CurrentMoveDirection = dir.x > 0 ? 1f : -1f;
-            transform.Find("Sprite").GetComponent<SpriteRenderer>().flipX = dir.x > 0 ? false : true;
-
-            // 예상 위치 계산 후 위치가 MinX와 MaxX 사이에 있는지 확인하고 조정
-            Vector3 nextPosition = transform.position + Vector3.right * _stat.MoveSpeed * CurrentMoveDirection * Time.deltaTime;
-            nextPosition.x = Mathf.Clamp(nextPosition.x, _stat.MinX, _stat.MaxX);
-
-            if (!_stat.IsStopOnTrack)
-                transform.position = nextPosition;
-        }
     }
 }

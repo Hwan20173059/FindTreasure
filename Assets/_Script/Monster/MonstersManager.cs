5c8e83ce0e860a2328cf4e80deebb1451de0cb64
EcchiClone <aa01092308481@gmail.com>
2024-03-04 15:17:53 +0900

    Feat. 몬스터 스폰 시 Y축 자동조정 및 X 이동범위 자동화 가능

--- Assets/_Script/Monster/MonstersManager.cs
+++ Assets/_Script/Monster/MonstersManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 // 몬스터 스폰과 관리, 상태관리
 public class MonstersManager : MonoBehaviour
@@ -74,6 +76,7 @@ public class MonstersManager : MonoBehaviour
             SpawnMonstersFromList(spawnInfo.spawnInfos);
         }
     }
+
     private void SpawnMonstersFromList(List<SpawnInfo> spawnData)
     {
         foreach (var spawnInfo in spawnData)
@@ -85,44 +88,92 @@ public class MonstersManager : MonoBehaviour
                 GameObject monsterObj = Instantiate(monsterPrefab, spawnInfo.spawnPosition, Quaternion.identity);
                 SpawnedMonsters.Add(monsterObj);
 
-                // "Ground"와 "Passthrough" 레이어에만 있는 물체를 감지하기 위한 LayerMask 생성
-                int layerMask = LayerMask.GetMask("Ground", "Passthrough");
+                // 현재 몬스터 Stat의 MinX, MaxX 등을 재설정하기 위해 준비
+                MonsterStat monsterStat = monsterObj.GetComponent<MonsterStat>();
 
-                // ray 쏘기
-                RaycastHit2D hit = Physics2D.Raycast(spawnInfo.spawnPosition, Vector2.down, 10f, layerMask);
-                if (hit.collider != null)
+                // 스폰 시 Y값 및, X범위를 바로 아래 플랫폼의 모양에 따라 재할당
+                if (spawnInfo.autoAdjustPosition)
                 {
-                    // 하단 콜라이더의 경계를 기반으로 MinX와 MaxX 계산
-                    BoxCollider2D monsterCollider = monsterObj.GetComponent<BoxCollider2D>();
-                    float colliderHalfWidth = monsterCollider != null ? monsterCollider.size.x * monsterObj.transform.localScale.x / 2f : 0;
-                    float minX = hit.collider.bounds.min.x + colliderHalfWidth;
-                    float maxX = hit.collider.bounds.max.x - colliderHalfWidth;
+                    // "Ground"와 "Passthrough" 레이어에만 있는 물체를 감지하기 위한 LayerMask 생성
+                    int layerMask = LayerMask.GetMask("Ground", "Passthrough");
 
-                    float genY = hit.collider.bounds.max.y;
-                    // 해결 될 때 까지 임시조치
-                    //monsterObj.transform.position = new Vector3(monsterObj.transform.position.x, genY, monsterObj.transform.position.z);
+                    // ray 쏘기
+                    RaycastHit2D hit = Physics2D.Raycast(spawnInfo.spawnPosition, Vector2.down, Mathf.Infinity, layerMask);
 
-                    // MonsterStat MinX, MaxX 설정, bool 관련 설정
-                    MonsterStat monsterStat = monsterObj.GetComponent<MonsterStat>();
-                    if (monsterStat != null)
+                    if (hit.collider != null)
                     {
-                        //monsterStat.MinX = minX; // 해결 될 때 까지 임시조치
-                        //monsterStat.MaxX = maxX;
+                        Vector3 monsterPosition = spawnInfo.spawnPosition;
 
-                        // 임시조치
-                        monsterStat.MinX = spawnInfo.spawnPosition.x - spawnInfo.tempMinX;
-                        monsterStat.MaxX = spawnInfo.spawnPosition.x + spawnInfo.tempMaxX;
+                        // 몬스터의 Y 위치 설정
+                        monsterPosition.y = hit.point.y;
+                        monsterObj.transform.position = monsterPosition;
 
-                        monsterStat.IsStopOnIdle = spawnInfo.isStopOnIdle;
-                        monsterStat.IsStopOnTrack = spawnInfo.isStopOnTrack;
+                        // 이동 범위 설정
+                        float leftBoundary = FindBoundary(monsterObj, Vector2.left, layerMask);
+                        float rightBoundary = FindBoundary(monsterObj, Vector2.right, layerMask);
+
+                        BoxCollider2D monsterCollider = monsterObj.GetComponent<BoxCollider2D>();
+                        float colliderHalfWidth = monsterCollider != null ? monsterCollider.size.x * monsterObj.transform.localScale.x / 2f : 0;
+                        monsterStat.MinX = Mathf.Min(spawnInfo.spawnPosition.x, leftBoundary + colliderHalfWidth);
+                        monsterStat.MaxX = Mathf.Max(rightBoundary - colliderHalfWidth, spawnInfo.spawnPosition.x);
+
                     }
                 }
+                // 스폰 시 위치 및 X범위를 수동으로 할당
+                else
+                {
+                    // x 이동 범위 수동 지정
+                    monsterStat.MinX = spawnInfo.spawnPosition.x - spawnInfo.tempMinX;
+                    monsterStat.MaxX = spawnInfo.spawnPosition.x + spawnInfo.tempMaxX;
+                }
+
+                monsterStat.IsStopOnIdle = spawnInfo.isStopOnIdle;
+                monsterStat.IsStopOnTrack = spawnInfo.isStopOnTrack;
+
             }
             else
             {
-                Debug.LogError($"NotFound : MonsterPrefab {spawnInfo.monsterType} | path {prefabPath}");
+                Debug.LogError($"프리팹({spawnInfo.monsterType})을 경로({prefabPath})에서 찾을 수 없음");
             }
         }
+    }
+
+    // 몬스터의 X이동범위 자동으로 할당할 경우 탐색
+    float FindBoundary(GameObject monster, Vector2 direction, int layerMask)
+    {
+        float step = 0.05f; // Raycast를 발사할 간격
+        float maxDistance = 20f; // 최대 탐색 거리
+        float rayLength = monster.GetComponent<BoxCollider2D>().size.y; // 몬스터 콜라이더의 높이
+
+        // 몬스터 하단에서 시작
+        Vector2 basePosition = new Vector2(monster.transform.position.x, monster.transform.position.y);
+        float goalPositionX = basePosition.x;
+
+        for (float distance = 0; distance <= maxDistance; distance += step)
+        {
+            Vector2 origin = basePosition + (direction * distance);
+            Vector2 rayStartBelow = origin + new Vector2(0, 0.01f); // 아래쪽 Ray 시작점
+            Vector2 rayStartAbove = origin + new Vector2(0, 0.1f); // 위쪽 Ray 시작점
+            // 시각화 FOR DEBUG
+            //Debug.DrawRay(rayStartBelow, Vector2.down * 0.05f, Color.red, 60f);
+            //Debug.DrawRay(rayStartAbove, Vector2.up * rayLength, Color.blue, 60f);
+
+            // 아랫방향으로 짧은 Raycast 발사
+            RaycastHit2D hitBelow = Physics2D.Raycast(rayStartBelow, Vector2.down, 0.05f, layerMask);
+
+            // 몬스터 위치보다 약간 위에서 위쪽으로 Raycast 발사
+            RaycastHit2D hitAbove = Physics2D.Raycast(rayStartAbove + new Vector2(0, 0.05f), Vector2.up, rayLength, layerMask);
+
+            // 아래에는 플랫폼이 있고, 위에는 플랫폼이 없는 경우
+            if (hitBelow.collider != null && !hitAbove.collider)
+            {
+                // 이동 가능한 경계를 찾음
+                goalPositionX = basePosition.x + direction.x * distance;
+            }
+            else break;
+        }
+
+        return goalPositionX;
     }
 
     private void DestroyAllMonsters()
